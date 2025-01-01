850a4b14d689865d888f7e13b68473d1c747a91a
baijidhossain <bayejidhossain47@gmail.com>
2025-01-01 10:57:24 +0600

    menu active

--- public/js/script.js
+++ public/js/script.js
@@ -5,6 +5,8 @@ links.forEach(link => {
     if (link.pathname === currentPath) {
         // Do something with the matching link
         console.log(`Found: ${link.href}`);
+        console.log(link.pathname)
+        console.log(currentPath)
         link.classList.add('active'); // Example: Add 'active' class
     }
 });
